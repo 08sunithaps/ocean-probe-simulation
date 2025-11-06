5316b53ab9e5a03fbc4dbed40e85b93b9cc47ba3
sunitha <08sunithaps@gmail.com>
2025-11-06 22:04:55 +0530

    small correction

--- src/test/java/org/example/ProbeSimulatorTest.java
+++ src/test/java/org/example/ProbeSimulatorTest.java
@@ -15,11 +15,7 @@ public class ProbeSimulatorTest {
         ProbeSimulator.Grid grid = new ProbeSimulator.Grid(5, 5, Set.of());
         ProbeSimulator.Position start = new ProbeSimulator.Position(1, 1, ProbeSimulator.Direction.NORTH);
         List<Character> cmds = List.of('F');
-
-
         List<String> result = ProbeSimulator.simulate(grid, start, cmds);
-
-
         assertTrue(result.contains("Final:1,2,NORTH"), "Expected final position to be (1,2) facing NORTH");
     }
 
